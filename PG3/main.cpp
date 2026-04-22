2cfc8ef7cd7730335eca222425a1988ae3beca14
NyanToeSat <k025g1034@g.neec.ac.jp>
2026-04-22 15:23:36 +0900

    commit

--- PG3/main.cpp
+++ PG3/main.cpp
@@ -1,10 +1,24 @@
-#include <iostream>
-#include <CoreWindow.h>
+#include <stdio.h>
+#include <Windows.h>
+
+
+template <typename Type>
+Type Min(Type a, Type b) {
+	return (a < b) ? a : b;
+}
+
+template <>
+char Min<char>(char a, char b) {
+	printf("数字以外代入できません\n");
+	return 0;
+}
 
 int main() {
-	SetConsoleOutputCP(65001);
-	char str[] = "ニャン";
-	printf("%s",str);
-	return 0;
 
+	SetConsoleOutputCP(65001);
+	printf("%d\n", Min<int>(114, 514));
+	printf("%f\n", Min<float>(3.14, 2.71));
+	printf("%lf\n",Min<double>(1.41, 2.71));
+	printf("%c\n", Min<char>('N', 'Y'));
+	return 0;
 }
