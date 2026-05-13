95a0e07500f4f8207131bfac5e3497833e532f05
NyanToeSat <k025g1034@g.neec.ac.jp>
2026-05-13 14:58:14 +0900

    commit

--- PG3/main.cpp
+++ PG3/main.cpp
@@ -9,12 +9,12 @@ Type Min(Type a, Type b) {
 
 template <>
 char Min<char>(char a, char b) {
-	printf("数字以外代入できません\n");
+	printf("数字以外は代入できません\n");
 	return 0;
 }
 
 int main() {
-
+	system("chcp 65001>nul");
 	SetConsoleOutputCP(65001);
 	printf("%d\n", Min<int>(114, 514));
 	printf("%f\n", Min<float>(3.14, 2.71));
