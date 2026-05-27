7ca56dd38dd04f5eaca1d8a416fd80dd6efee088
NyanToeSat <k025g1034@g.neec.ac.jp>
2026-05-27 15:57:59 +0900

    commit

--- PG3/main.cpp
+++ PG3/main.cpp
@@ -1,10 +1,39 @@
 #include <iostream>
-#include <CoreWindow.h>
+#include <ctime>
+#include <Windows.h>
+
+using namespace std;
+
+typedef void (*Callback)();
+
+void showCorrect() {
+    cout << "正解" << endl;
+}
+
+void showWrong() {
+    cout << "不正解" << endl;
+}
+
+void judge(int dice, int user, Callback cb) {
+    Sleep(3000);
+    Callback results[2] = { showWrong, showCorrect };
+    cb = results[dice % 2 == user];
+    cb();
+}
 
 int main() {
-	SetConsoleOutputCP(65001);
-	char str[] = "ニャン";
-	printf("%s",str);
-	return 0;
+    SetConsoleOutputCP(65001);
+    srand((unsigned)time(NULL));
 
+    int user;
+    cout << "奇数なら1、偶数なら0を入力: ";
+    cin >> user;
+
+    int dice = rand() % 6 + 1;
+    cout << "出目は " << dice << endl;
+
+    Callback cb = NULL;
+    judge(dice, user, cb);
+
+    return 0;
 }
